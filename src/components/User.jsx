b9c00d45f3171c93ee6be07dd0dac66819de371f
Алексей Кузнецов <alexeykuznetsov@MacBook-Air-Aleksej.local>
2022-04-10 16:26:02 +0300

    refresh code

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -14,7 +14,7 @@ const User = (props) => {
 
       <td>{user.profession.name}</td>
       <td>{user.completedMeetings}</td>
-      <td>{user.rate}</td>
+      <td>{user.rate}/5</td>
       <td>
         <BookMark
           status={user.bookmark}
